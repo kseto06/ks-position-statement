68c7114e374b9a87a6b69f4afb24e013863c5365
kseto06 <kaden.seto06@gmail.com>
2025-01-11 23:39:08 -0500

    feat: tailwind fade-in + typing animations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,42 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      animation: {
+        fadeIn: "fadeIn 2s ease-in-out", // fadeIn animation
+        typewriter: "typewriter 3s steps(1000) forwards", //typing cursor animation
+        cursor: 'typewriter 3s steps(1000) forwards, blink 3s steps(1000) infinite 2s',
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0" },   // invisible at opacity = 0
+          "100%": { opacity: "1" },  // visible at opacity = 1
+        },
+        typewriter: {
+          to: {
+            left: "100%"
+          }
+        },
+        blink: {
+          '0%': {
+            opacity: '0',
+          },
+          '0.1%': {
+            opacity: '1',
+          },
+          '50%': {
+            opacity: '1',
+          },
+          '50.1%': {
+            opacity: '0',
+          },
+          '100%': {
+            opacity: '0',
+          },
+        },
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif']
+      }
     },
   },
   plugins: [],

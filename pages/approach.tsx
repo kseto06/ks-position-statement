d2ec6c0483a0185d5f122b88c48a604e0579986e
kseto06 <kaden.seto06@gmail.com>
2025-01-12 17:39:17 -0500

    disable: unescaped entities rule

--- pages/approach.tsx
+++ pages/approach.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-unescaped-entities */
+
 import '../app/globals.css' //Apply consistent style
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";

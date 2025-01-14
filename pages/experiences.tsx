f2f20ce8e041a85a40e915086e439fcc3e6d29ae
kseto06 <kaden.seto06@gmail.com>
2025-01-13 20:15:20 -0500

    feat: initial experiences page

--- pages/experiences.tsx
+++ pages/experiences.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-unescaped-entities */
+
 import '../app/globals.css' //Apply consistent style
 import { Geist, Geist_Mono } from "next/font/google";
 
@@ -17,14 +19,51 @@ const Experiences = () => {
         <main className="flex flex-col gap-8 row-start-2 items-start sm:items-start">
             {/* Title */}
             <div className={geistSans.className}>
-                <h1 className="text-inside text-decimal text-xl sm:text-2xl font-[family-name:var(--font-geist-sans)] max-w-lg mx-auto relative w-[max-content] before:absolute before:inset-0 before:animate-typewriter before:bg-black after:absolute after:inset-0 after:w-[0.125em] after:animate-cursor after:bg-white">
+                <h1 
+                    className="text-inside text-decimal text-xl sm:text-2xl font-[family-name:var(--font-geist-sans)] max-w-lg mx-auto">
                     Engineering Design Influenced by Experiences
                 </h1>
             </div>
+
+            <div className={geistSans.className}>
+                <h3 
+                    className="text-inside text-decimal text-lg sm:text-lg font-[family-name:var(--font-geist-sans)] mx-auto relative w-[max-content] before:absolute before:inset-0 before:animate-typewriter before:animation-delay-[2s] before:bg-black after:absolute after:inset-0 after:w-[0.125em] after:animate-cursor after:bg-white">
+                    I am a specialist in robotics, software, and AI/Machine Learning engineering.
+                </h3>
+            </div>
+
+            <div style={{ display: 'flex', gap: '10px', justifyContent: "center", alignItems: 'center'}}>
+                <figure style={{ textAlign: 'center' }}>
+                    <img src="robotics.png" style={{ width: '700px', height: 'auto' }} />
+                    <figcaption className={geistSans.className}>Representing and competing on the field for my Robotics team, Dec. 2022</figcaption>
+                </figure>
+                <figure style={{ textAlign: 'center' }}> 
+                    <img src="github.png" style={{ width: '500px', height: 'auto' }} /> 
+                    <figcaption className={geistSans.className}>Software development project portfolio</figcaption>
+                </figure>
+            </div>
+
             {/* Text */}
             <div className={geistMono.className}>
                 <p className="text-inside text-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-
+                    My many engineering extracurricular experiences includes Robotics team programming lead, software developer positions, 
+                    and most recently a reinforcement learning lead developer in UofT’s Machine Intelligence Student Team. My continuous passion for exploring these 
+                    fields have heavily influenced by perspective on how I approach engineering.
+                    <br />
+                    <br />
+                    In my engineering experiences, I always have to tackle high-stake problems. In my Robotics team, I led developers and created complex programs
+                    with the hope of being able to qualify for the FIRST Tech Challenge Robotics World Championships in Houston. Currently, I am leading 
+                    UTMIST's prime reinforcement learning project to develop a tournament/hackathon for over 250 participants/stakeholders. I interpret engineering design 
+                    as a powerful tool where the concepts learned can be applied to show a vision of what I want to create, and how to layout and combine the pieces to get to an ultimate goal. 
+                    For example, being UTMIST's lead developer, I am constantly applying NGO frameworks to keep my team on the scope of the project and FDCR to brainstorm and implement ideas in code.
+                    <br />
+                    <br />
+                    Since I am constantly tackling large problems, I engage in engineering design always trying to think ahead of what I want my end goal to be. Since I perceive engineering design as a
+                    tool to model a vision, I believe that engineers should not be constrained to strictly following the FDCR model (i.e. I don't have to go through the framing process first, before I can start diverging). 
+                    I am constantly diverging and converging of what I want to design and produce for a project, even at the framing stage when I am only finding an opportunity; this allows me to think of an ultimate goal 
+                    that I want to achieve and it provides me with better motivation to tackle and finish a project. For example, in my community pitch I identify cyclists in downtown Toronto who are at risk of injuries
+                    due to reckless driving. When I was ideating and framing this opportunity, I was already brainstorming ideas and designs in areas that I am interested in, such as making a computer vision to improve
+                    the safety of the bikers. 
                 </p>
             </div>
         </main>
